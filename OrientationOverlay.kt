edae5929cdc4d9b1cc75b75910b6527d90b0c481
asilyanov <asilyanov@npomis.ru>
2025-05-22 11:40:08 +0000

    Add needed folders and fiels

---
+++ OrientationOverlay.kt
@@ -0,0 +1,12 @@
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from orient-any-mobile!")
+
+
+if __name__ == "__main__":
+    main()
